b72f582e5d67d9ce6fd03d24c76660f0f043b117
Uskova Maria <uskovamaria16@gmail.com>
2023-09-16 20:28:58 +0200

    hw4

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let products = [
+    ['Хліб', 20],
+    ['Масло', 70],
+    ['Ковбаса', 150],
+    ['Цукор', 15]
+];
+let sum = 0;
+
+for (let i = 0; i < products.length; i++) {
+    let name = products[i][0];
+    let price = products[i][1];
+
+    if (price <= 20) {
+        console.log(i + 1,name, price, ':)');
+    } else {
+        console.log(i + 1,name, price, ':(');  
+    }   
+
+    sum +=price;
+    console.log(sum);  
+}
+
+
+
+
